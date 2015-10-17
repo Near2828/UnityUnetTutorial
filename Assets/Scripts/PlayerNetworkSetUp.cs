6f2a639364bda1f3e3f1c4b61e118eb492cfd98c
Near2828 <sollock2828@gmail.com>
2015-10-18 02:57:57 +0900

    Standard Asset追加。 NetworkManger追加。

---
+++ Assets/Scripts/PlayerNetworkSetUp.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerNetworkSetUp : NetworkBehaviour {
+
+    #region PrivateMember
+
+    [SerializeField]
+    Camera m_FPSCharaCamera;
+
+    [SerializeField]
+    AudioListener m_AudioListener;
+
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        if (isLocalPlayer) {
+            GameObject.Find("Scene Camera").SetActive(false);
+            GetComponent<CharacterController>().enabled = true;
+            GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;
+            m_FPSCharaCamera.enabled = true;
+            m_AudioListener.enabled = true;
+        }
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
